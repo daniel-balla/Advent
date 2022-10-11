4393fdf722dff9d6baceb6069cc0929be8854f51
daniel-balla <daballa@deloitte.com>
2022-10-11 10:15:26 +0200

    New method count1 using Files.readAllBytes

--- src/Day_1/Part_1.java
+++ src/Day_1/Part_1.java
@@ -2,6 +2,7 @@ package Day_1;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.nio.file.*;
 
@@ -10,7 +11,7 @@ public class Part_1 {
 
 		Path path = Paths.get("src/Day_1/day_1_input");
 		System.out.println("Santa needs to go to floor " + count(path));
-
+		System.out.println("Santa needs to go to floor " + count1(path));
 	}
 
 	public static long count(Path path) throws FileNotFoundException, IOException {
@@ -26,6 +27,24 @@ public class Part_1 {
 			count2 = lines2.flatMapToInt(s -> s.chars()).filter(e -> e == ')').count();
 		}
 
+		counter = count1 - count2;
+
+		return counter;
+	}
+
+	public static long count1(Path path) throws FileNotFoundException, IOException {
+
+		long counter = 0, count1 = 0, count2 = 0;
+
+		byte[] arr = Files.readAllBytes(path);
+		String str = new String(arr);
+		
+		IntStream lines1 = str.chars();
+		IntStream lines2 = str.chars();
+		
+		count1 = lines1.filter(e -> e == '(').count();
+		count2 = lines2.filter(e -> e == ')').count();
+		
 		counter = count1 - count2;
 
 		return counter;
